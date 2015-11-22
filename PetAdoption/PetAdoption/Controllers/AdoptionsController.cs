1667d22302528399819ddc78c4bd2f1f1c97cd86
flaviadocouto <flaviadocouto@hotmail.com>
2015-11-22 19:23:19 +0000

    creating controllers

---
+++ PetAdoption/PetAdoption/Controllers/AdoptionsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PetAdoption.Models;
+
+namespace PetAdoption.Controllers
+{
+    public class AdoptionsController : Controller
+    {
+        private PetAdoptionModelContext db = new PetAdoptionModelContext();
+
+        // GET: Adoptions
+        public ActionResult Index()
+        {
+            return View(db.Adoptions.ToList());
+        }
+
+        // GET: Adoptions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Adoption adoption = db.Adoptions.Find(id);
+            if (adoption == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adoption);
+        }
+
+        // GET: Adoptions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Adoptions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AdoptionID,Date")] Adoption adoption)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Adoptions.Add(adoption);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(adoption);
+        }
+
+        // GET: Adoptions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Adoption adoption = db.Adoptions.Find(id);
+            if (adoption == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adoption);
+        }
+
+        // POST: Adoptions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AdoptionID,Date")] Adoption adoption)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(adoption).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(adoption);
+        }
+
+        // GET: Adoptions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Adoption adoption = db.Adoptions.Find(id);
+            if (adoption == null)
+            {
+                return HttpNotFound();
+            }
+            return View(adoption);
+        }
+
+        // POST: Adoptions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Adoption adoption = db.Adoptions.Find(id);
+            db.Adoptions.Remove(adoption);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
