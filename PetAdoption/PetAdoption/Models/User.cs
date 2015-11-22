a56baef8d87024003c4bbd6dd86fffadc5b51faa
flaviadocouto <flaviadocouto@hotmail.com>
2015-11-22 21:26:54 +0000

    display menu

---
+++ PetAdoption/PetAdoption/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetAdoption.Models
+{
+    public class User
+    {
+        private ICollection<Adoption> _adoptions;
+
+        public User()
+        {
+            _adoptions = new List<Adoption>();
+        }
+
+
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Zipcode { get; set; }
+        public EmailService Email { get; set; }
+
+        public virtual ICollection<Adoption> Adoption
+        {
+            get { return _adoptions; }
+            set { _adoptions = value; }
+        }
+    }
+}
