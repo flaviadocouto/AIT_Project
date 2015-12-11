a8e52176d04ae9d33387ba9bdb5af6b172be2d2d
flaviadocouto <flaviadocouto@hotmail.com>
2015-12-11 12:47:33 +0000

    add web service

---
+++ PetAdoption/PetAdoption/PetAdoptionService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="PetAdoptionService.asmx.cs" Class="PetAdoption.PetAdoptionService" %>
---
+++ PetAdoption/PetAdoption/PetAdoptionService.asmx.cs
@@ -0,0 +1,73 @@
+﻿using PetAdoption.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace PetAdoption
+{
+    /// <summary>
+    /// Summary description for PetAdoptionService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class PetAdoptionService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        private PetAdoptionModelContext entities = new PetAdoptionModelContext();
+
+        [WebMethod]
+        public string[] GetDogsByName(string name)
+        {
+            List<string> animals = new List<string>();
+
+            foreach (var dog in entities.Animals
+                                         .Where(b => b.Name.Contains(name))
+                                         .ToList())
+            {
+                animals.Add(dog.Name);
+            }
+            return animals.ToArray();
+        }
+
+        [WebMethod]
+        public string[] GetShelterByName(string name)
+        {
+            List<string> shelters = new List<string>();
+
+            foreach (var shelter in entities.Shelters
+                                         .Where(b => b.Name.Contains(name))
+                                         .ToList())
+            {
+                shelters.Add(shelter.Name);
+            }
+            return shelters.ToArray();
+        }
+
+        [WebMethod]
+        public string[] GetBreedByName(string name)
+        {
+            List<string> breeds = new List<string>();
+
+            foreach (var breed in entities.Breeds
+                                         .Where(b => b.BreedName.Contains(name))
+                                         .ToList())
+            {
+                breeds.Add(breed.BreedName);
+            }
+            return breeds.ToArray();
+        }
+    }
+
+    
+}
